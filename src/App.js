cc3e5420134a966e69bfbba39d05dddb13b023b0
O Hung Lun <hunglun.o@gmail.com>
2023-11-25 03:25:01 +0000

    remove unused line

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Profile from './components/Profile'; // Adjust the path based on your file structure
 import PortfolioSection from './components/PortfolioSection'; // Import the PortfolioSection component
 import SingaporeLocationTimeline from './components/SingaporeLocationTimeline';
-import TimelineComponent from './components/TimelineComponent';
 function App() {
   return (
     <div className="App">
