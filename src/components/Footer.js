a0a387492689137b15ba7d68cb57034f516e8d85
O Hung Lun <hunglun.o@gmail.com>
2023-12-11 23:13:34 +0000

    change title and email

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
       <p>© 2023 婉煊教育</p>
-      <p>Contact Us: info@wanxuaneducation.com</p>
+      <p>Contact Us: admin@wanxuaneducation.com</p>
     </footer>
   );
 }
