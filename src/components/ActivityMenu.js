b41769e975797ee79e5bbc071f5852429f22c678
O Hung Lun <hunglun.o@gmail.com>
2023-12-23 13:29:30 +0000

    added photos for activity menu

--- src/components/ActivityMenu.js
+++ src/components/ActivityMenu.js
@@ -1,17 +1,23 @@
 // ActivityMenu.js
 import React from 'react';
 import attractions from './attractions.js'; // Adjust the path as necessary
-
+import './ActivityMenu.css';
 function ActivityMenu() {
   return (
     <div>
-      <h2>活动菜单</h2>
+      <h1>活动菜单</h1>
       <ul>
         {attractions.map((attraction, index) => (
           <li key={index}>
-            <a href={attraction.url} target="_blank" rel="noopener noreferrer">
-             {index + 1} {attraction.name} 
-            </a>
+            <div class="image-container">
+              <img src={ process.env.PUBLIC_URL + '/' + attraction.image} alt={attraction.name} />
+              <div class="overlay-text">
+                <a href={attraction.url} target="_blank" rel="noopener noreferrer">
+                 {index + 1} {attraction.name} 
+                </a>
+              </div>
+            </div>
+        
           </li>
         ))}
       </ul>
--- src/components/attractions.js
+++ src/components/attractions.js
@@ -1,23 +1,23 @@
 // attractions.js
 const attractions = [
-    { name: '樟宜机场', date: '2024-01-22', url: 'https://www.changiairport.com' },
-    { name: '滨海湾花园', date: '2024-01-23 上午', url: 'https://www.gardensbythebay.com.sg' },
-    { name: '万象馆', date: '2024-01-23', url: 'https://www.madametussauds.com/singapore' },
-    { name: '鱼尾狮', date: '2024-01-23', url: 'https://www.visitsingapore.com.cn/see-do-singapore/recreation-leisure/viewpoints/merlion-park/' },
-    { name: '滨海湾金沙', date: '2024-01-23', url: 'https://zh.marinabaysands.com' },
-    { name: '风动飞行', date: '2024-01-24', url: 'https://www.iflysingapore.com/activity-to-do-in-singapore/?gad_source=1&gclid=CjwKCAiA04arBhAkEiwAuNOsIgDqVc08q0BkHy1tm-x7FDFXNUmUeEPuv9CBzCQMrg46jyigvhAKRxoCAo4QAvD_BwE' },
-    { name: '新加坡游船', date: '2024-01-24', url: 'https://www.visitsingapore.com.cn/see-do-singapore/recreation-leisure/fun-things-to-do/singapore-river-bumboat/' },
-    { name: '夜间动物园', date: '2024-01-24', url: 'https://www.mandai.com/zh/night-safari.html' },
-    { name: '圣淘沙', date: '2024-01-25', url: 'https://www.sentosa.com.sg/' },
-    { name: '小印度', date: '2024-01-25', url: 'https://www.visitsingapore.com/see-do-singapore/places-to-see/little-india/' },
-    { name: '牛车水', date: '2024-01-25', url: 'https://www.visitsingapore.com.cn/see-do-singapore/places-to-see/chinatown/' },
-    { name: '摩天轮', date: '2024-01-25', url: 'https://www.singaporeflyer.com/cn' },
-    { name: '科学馆', date: '2024-01-26', url: 'https://www.science.edu.sg' },
-    { name: '星耀樟宜', date: '2024-01-26', url: 'https://www.jewelchangiairport.com/en.html' },
-    { name: '深林学习', date: '2024-01-26', url: 'https://forestschoolsingapore.com' },
-    { name: '滑雪', date: '2024-01-26', url: 'https://trifectasingapore.com' },
-    { name: '滑板', date: '2024-01-26', url: 'https://www.nparks.gov.sg/juronglakegardens/explore-our-gardens/attractions/skatepark-@-lakeside-garden' },
-
+    { image: 'dragonfly-and-kingfisher-lakes-main.jpg', name: '滨海湾花园', date: '2024-01-23 上午', url: 'https://www.gardensbythebay.com.sg' },
+    { image: 'madame-tussauds-singapore.webp', name: '万象馆', date: '2024-01-23', url: 'https://www.madametussauds.com/singapore' },
+    { image: 'merlion-park.jpeg', name: '鱼尾狮', date: '2024-01-23', url: 'https://www.visitsingapore.com.cn/see-do-singapore/recreation-leisure/viewpoints/merlion-park/' },
+    { image: 'dragonfly-and-kingfisher-lakes-main.jpg', name: '滨海湾金沙', date: '2024-01-23', url: 'https://zh.marinabaysands.com' },
+    { image: 'ifly.webp', name: '风动飞行', date: '2024-01-24', url: 'https://www.iflysingapore.com/activity-to-do-in-singapore/?gad_source=1&gclid=CjwKCAiA04arBhAkEiwAuNOsIgDqVc08q0BkHy1tm-x7FDFXNUmUeEPuv9CBzCQMrg46jyigvhAKRxoCAo4QAvD_BwE' },
+    { image: 'river-cruise.webp', name: '新加坡游船', date: '2024-01-24', url: 'https://www.visitsingapore.com.cn/see-do-singapore/recreation-leisure/fun-things-to-do/singapore-river-bumboat/' },
+    { image: 'night-safari.webp', name: '夜间动物园', date: '2024-01-24', url: 'https://www.mandai.com/zh/night-safari.html' },
+    { image: 'sentosa.webp', name: '圣淘沙', date: '2024-01-25', url: 'https://www.sentosa.com.sg/' },
+    { image: 'little-india.webp', name: '小印度', date: '2024-01-25', url: 'https://www.visitsingapore.com/see-do-singapore/places-to-see/little-india/' },
+    { image: 'chinatown.jpeg', name: '牛车水', date: '2024-01-25', url: 'https://www.visitsingapore.com.cn/see-do-singapore/places-to-see/chinatown/' },
+    { image: 'singapore-flyer.webp', name: '摩天轮', date: '2024-01-25', url: 'https://www.singaporeflyer.com/cn' },
+    { image: 'science-centre.webp', name: '科学馆', date: '2024-01-26', url: 'https://www.science.edu.sg' },
+    { image: 'jewel.webp', name: '星耀樟宜', date: '2024-01-26', url: 'https://www.jewelchangiairport.com/en.html' },
+    { image: 'rifle-range-park.jpeg', name: '深林学习', date: '2024-01-26', url: 'https://forestschoolsingapore.com' },
+    { image: 'snowboard.jpeg', name: '滑雪', date: '2024-01-26', url: 'https://trifectasingapore.com' },
+    { image: 'skateboard-park.jpg', name: '滑板', date: '2024-01-26', url: 'https://www.nparks.gov.sg/juronglakegardens/explore-our-gardens/attractions/skatepark-@-lakeside-garden' },
+    { image: 'chaos-lab.jpeg', name: '混乱实验室', until : '2024-01-29', url: 'https://chaos-lab.co/singapore' },
+    
 ];
 
 export default attractions;
