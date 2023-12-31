b1904eb9063f1bee6dd2b326954ea5cba92a5244
O Hung Lun <hunglun.o@gmail.com>
2023-12-31 09:26:25 +0000

    disable 2 header items

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,8 @@ function Header(  { selectPage }) {
       <nav>
         <li onClick={() => selectPage(0)}>首页</li>
         {/* Attach the onClick event handler to this li */}
-        <li onClick={() => selectPage(3)}>留学</li>
-        <li onClick={() => selectPage(2)}>兴趣班</li>
+        <li onClick={() => selectPage(3)}><button disabled>留学</button></li>
+        <li onClick={() => selectPage(2)}><button disabled>兴趣班</button></li>
         <li onClick={() => selectPage(1)}>游学活动</li>
   
       </nav>
